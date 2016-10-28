128755b721d39fdf36e5c19e43976ec7a3e9f8e5
broerjuang <juangwiantoro@gmail.com>
2016-10-28 21:19:07 +0700

    initate express

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 'use strict'
 
+//---------------------------
+// Node Modules
+//---------------------------
+
+// Express Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Initiate Express
+const app = express();
+const router = app.Router() ;
+
+//---------------------------
+//App Configuration
+//---------------------------
