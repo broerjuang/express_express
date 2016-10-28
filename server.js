d4c9ce2e6e5dbc589af54d4203e9fa06622e47c8
broerjuang <juangwiantoro@gmail.com>
2016-10-28 22:39:13 +0700

    router .post .delete .put

--- server.js
+++ server.js
@@ -28,10 +28,38 @@ const books = require('./data.js');
 // Route Configuration
 //---------------------------
 
+
 router.get('/books', (req, res) => {
   res.send(books)
 });
 
+router.post('/books', (req, res) => {
+  let book = {
+    id : Number(req.body.id),
+    title : req.body.title,
+    price : Number(req.body.price)
+  }
+
+  // push the book
+  books.push(book)
+  res.json(book)
+})
+
+// router.get('/books/:id', (req, res) => {
+//   const book = books.filter(book => book.id === req.params.id)
+// })[0]
+//   res.json(book)
+// })
+
+
+router.delete('/books/:id', (req, res) => {
+  // get book by id
+  const book = books.filter(book => {return book.id === Number(req.params.id)})
+  // delete the book by id
+  books.splice(books.indexOf(book), 1)
+  // send success message
+  res.json({'message' : `Book ${id} has been deleted`});
+})
 //---------------------------
 // Register Routes
 //---------------------------
