00ed2b776ed2b909a22484ca86b5798693322a6e
broerjuang <juangwiantoro@gmail.com>
2016-11-02 15:32:00 +0700

    change the file's name to make it work

---
+++ server/data/books.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const data = [
+  {
+    id      : 1,
+    title   : 'learn express',
+    price   : 200
+  },
+  {
+    id      : 2,
+    title   : 'learn mongodb',
+    price   : 90
+  },
+  {
+    id      : 3,
+    title   : 'learn nodejs',
+    price   : 20
+  },
+  {
+    id      : 4,
+    title   : 'learn math',
+    price   : 300
+  },
+  {
+    id      : 5,
+    title   : 'learn biology',
+    price   : 400
+  }
+]
+
+
+module.exports = data
