e1b952adf2506ca66febfaec9deed896fc48540f
broerjuang <juangwiantoro@gmail.com>
2016-11-03 05:37:59 +0700

    adding user schema

--- server/models/model.users.js
+++ server/models/model.users.js
@@ -1,1 +1,23 @@
-'use strict'
+const mongoose = require('mongoose');
+const localMongose = require('passport-local-mongoose');
+
+const UserSchema = new mongoose.Schema({
+  sureName    : String,
+  userName    : String,
+  mobile      : {
+    type    : String,
+    unique  : true
+  },
+  email       : {
+    type    : String,
+    required: true,
+    unique  : true
+  },
+  password    : String,
+  birthday    : String
+}, {
+  timestamp : true
+})
+
+UserSchema.plugin(localMongose)
+module.exports = mongoose.Model('User', UserSchema)
